a90278f30b8d1e021f83be427e14d32acf831ac4
Arkadiusz Lis <areklis909@gmail.com>
2020-02-22 14:19:03 +0100

    remove error_logs

--- server/include/ChartCleaner.php
+++ server/include/ChartCleaner.php
@@ -11,12 +11,10 @@ class ChartCleaner {
 
     function __construct($chart_name) {
         $this->chart_to_remove = realpath(self::chart_path) . self::slash . $chart_name;
-        error_log($this->chart_to_remove);
     }
 
     function __destruct() {
         $command = self::rm_command . $this->chart_to_remove;
-        error_log($command);
         exec($command);
     }
 }
