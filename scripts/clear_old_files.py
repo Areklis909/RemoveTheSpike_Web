bc6dd68219d81d6d0ab0eb289a55889435c1ae8d
Arkadiusz Lis <areklis909@gmail.com>
2020-03-10 21:47:02 +0100

    update clear_old_files script, add unit option and fix path issue

--- scripts/clear_old_files.py
+++ scripts/clear_old_files.py
@@ -5,12 +5,21 @@ from stat import *
 
 path = sys.argv[1]
 older_then_in_hours = int(sys.argv[2])
-older_then_in_seconds = older_then_in_hours * 60 * 60
+
+older_then_in_seconds = older_then_in_hours
+if '--minutes' in sys.argv:
+    older_then_in_seconds = older_then_in_seconds * 60
+elif '--hours' in sys.argv:
+    older_then_in_seconds = older_then_in_seconds * 60 * 60
+else:
+    older_then_in_seconds = older_then_in_seconds * 60 * 60
+
 
 files_in_directory = os.listdir(path)
 
 for file in files_in_directory:
-    file_metadata = os.stat(file)
+    fullpath = path + '/' + file
+    file_metadata = os.stat(fullpath)
     last_access_time = file_metadata[ST_ATIME]
     if(time.time() - last_access_time > older_then_in_seconds):
-        os.remove(path + '/' + file)
+        os.remove(fullpath)
