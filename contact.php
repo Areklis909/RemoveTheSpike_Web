c5f6b509a4beb9d8f3fcb2434031b68d6791d19b
Arkadiusz Lis <areklis909@gmail.com>
2020-03-12 23:26:33 +0100

    Add email hyperlink

--- contact.php
+++ contact.php
@@ -9,7 +9,9 @@
 				<h2>Contact</h2>
 				<p>
 					Should you encounter any troubles with the website or the algorithm itself (like deterioration of the quality),
-					please let me know: remove_the_spike@interia.pl
+					please let me know: <a href="mailto:remove_the_spike@interia.pl">remove_the_spike@interia.pl</a>
+				</p>
+				<p>
 					If you have a recording that causes troubles, please send it as well.
 				</p>
 			</div>
